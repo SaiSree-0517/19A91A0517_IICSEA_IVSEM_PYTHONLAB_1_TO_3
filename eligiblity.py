2b81022075527a949d288044fdebb508892568fa
SaiSree-0517 <69571289+SaiSree-0517@users.noreply.github.com>
2021-05-16 14:32:44 +0530

    Add files via upload

---
+++ eligiblity.py
@@ -0,0 +1,9 @@
+"""
+Implement a python script for checking whether the citizen is eligible for vote or not
+"""
+age=int(input("enter age :"))
+if age>=18:
+    print("Citizen is eligible for voting!")
+else:
+    print("Citizen not eligible for voting!")
+    
