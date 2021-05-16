07bde630f8cbf83e6676236e9906cf2a2fe6d538
SaiSree-0517 <69571289+SaiSree-0517@users.noreply.github.com>
2021-05-16 14:32:06 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,15 @@
+"""
+Implement a python script add.py that takes 2
+numbers as command line arguments and perform arithmetic operations on them.
+
+"""
+import sys
+n1=int(sys.argv[1])
+n2=int(sys.argv[2])
+print('Sum=',(n1+n2))
+print('Sub=',(n1-n2))
+print('Div=',(n1/n2))
+print('Floordiv=',(n1//n2))
+print('Mul=',(n1*n2))
+print('exp=',(n2**2))
+print('Mod=',(n1%n2))
