bf47ff16c1ffcb1a2830432e29120baff05233b7
SaiSree-0517 <69571289+SaiSree-0517@users.noreply.github.com>
2021-05-16 15:15:20 +0530

    Update and rename distance.py to 2.1experiment.py

--- 2.1experiment.py
+++ 2.1experiment.py
@@ -12,4 +12,10 @@ y2=int(input("enter y2 : "))
 
 result= ((((x2 - x1 )**2) + ((y2-y1)**2) )**0.5)
 
-print("distance between",(x1,x2),"and",(y1,y2),"is : ",result)
+print("distance between",(x1,y1),"and",(x2,y2),"is : ",result)
+#output
+enter x1 : 5
+enter x2 : 6
+enter y1 : 7
+enter y2 : 8
+distance between (5, 7) and (6, 8) is :  1.4142135623730951
